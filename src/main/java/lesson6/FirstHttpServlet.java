0436f455e6e305adc12824c9b734417489846630
mpasichniuk <marypasichnyuk@gmail.com>
2023-02-23 17:25:01 +0100

    6

--- src/main/java/lesson6/FirstHttpServlet.java
+++ src/main/java/lesson6/FirstHttpServlet.java
@@ -12,7 +12,7 @@ public class FirstHttpServlet extends HttpServlet {
 
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            resp.getWriter().println("Hello World");
+            resp.getWriter().println("Hello World!");
         }
     }
 
